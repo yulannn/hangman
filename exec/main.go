45f55191af45a496116cc62bb9b3a7af725f2ef2
Enzo <enzo.carbonara@ynov.com>
2023-10-12 11:53:29 +0200

    fix merge

--- exec/main.go
+++ exec/main.go
@@ -1,27 +1,8 @@
 package main
 
-import "fmt"
+import "hangman"
 
 func main() {
-	var filename string
-		fmt.Print("Entrez le nom du fichier: ")
-		fmt.Scanln(&filename)
-		file, aled := os.Open(filename)
-		if aled != nil {
-			fmt.Println("Erreur:", aled)
-		} else {
-			defer file.Close()
-			data := make([]byte, 1024)
-			for {
-				n, aled := file.Read(data)
-				if aled == io.EOF {
-					break
-				}
-				if aled != nil {
-					fmt.Println("Erreur lecture du fichier", aled)
-					break
-				}
-				fmt.Print(string(data[:n]))
-			}
-		}
+    hangman.Affiche()
+	hangman.RandomWord()
 }
