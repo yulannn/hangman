9cce7bd50e866e4341fdd7d2591c720a529c2cf7
Enzo <enzo.carbonara@ynov.com>
2023-10-12 17:33:43 +0200

    fix chance / choix lettre, mot

--- exec/main.go
+++ exec/main.go
@@ -19,30 +19,59 @@ func main() {
 	fmt.Println("Bienvenue au jeu du pendu !")
 
 	for chance := 0; chance < chance_max; chance++ {
-		var lettre string
-		fmt.Print("Entrez une lettre : ")
-		fmt.Scanln(&lettre)
+		var devinerMotOuLettre string
+        fmt.Printf("Il vous reste %d tentatives /n", chance_max)
+		fmt.Print("Voulez-vous deviner un mot complet ou une lettre ? (Mot/Lettre): ")
+		fmt.Scanln(&devinerMotOuLettre)
+		devinerMotOuLettre = strings.ToLower(devinerMotOuLettre)
 
-		lettre = strings.ToLower(lettre)
-		if len(lettre) != 1 || !isLowercaseLetter(lettre) {
-			fmt.Println("Veuillez entrer une seule lettre en minuscules.")
-			continue
+		if devinerMotOuLettre == "mot" {
+			var motDevine string
+			fmt.Print("Entrez le mot complet : ")
+			fmt.Scanln(&motDevine)
+
+			if motDevine == motSecret {
+				fmt.Printf("Félicitations ! Vous avez deviné le mot : %s\n", motSecret)
+				break
+			} else {
+				fmt.Println("Ce n'est pas le bon mot.")
+				// Réduire le nombre de chances restantes lorsque le mot complet est incorrect
+				chance_max -= 2
+				continue
+			}
+		} else if devinerMotOuLettre == "lettre" {
+			var lettre string
+			fmt.Print("Entrez une lettre : ")
+			fmt.Scanln(&lettre)
+
+			lettre = strings.ToLower(lettre)
+			if len(lettre) != 1 || !isLowercaseLetter(lettre) {
+				fmt.Println("Veuillez entrer une seule lettre en minuscules.")
+				continue
+                
+			}
+
+			if hangman.ContainsString(lettresDevinées, lettre) {
+				fmt.Println("Vous avez déjà deviné cette lettre.")
+				continue
+			}
+
+			lettresDevinées = append(lettresDevinées, lettre)
+
+            if !strings.Contains(motSecret, lettre) {
+				fmt.Println("La lettre n'est pas dans le mot.")
+				chance_max--
+            }
+
+			hangman.AfficherMot(motSecret, lettresDevinées)
+		} else {
+			fmt.Println("Répondez avec 'Mot' ou 'Lettre'.")
 		}
-
-		if hangman.ContainsString(lettresDevinées, lettre) {
-			fmt.Println("Vous avez déjà deviné cette lettre.")
-			continue
-		}
-
-		lettresDevinées = append(lettresDevinées, lettre)
-
-		hangman.AfficherMot(motSecret, lettresDevinées)
 
 		if chance == chance_max-1 {
 			fmt.Println("VOUS AVEZ PERDU")
-			fmt.Printf("LE MOT ETAIT %s", motSecret)
+			fmt.Printf("LE MOT ETAIT %s\n", motSecret)
 		}
-
 	}
 }
 
